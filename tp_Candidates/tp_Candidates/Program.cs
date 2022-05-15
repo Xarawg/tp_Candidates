c03836458747c5138d42a983309c78e06e59cd49
Xarawg <xarawg@gmail.com>
2022-05-15 17:10:25 +0500

    web api in ASP.NET 6, released: jwt and CRUD

---
+++ tp_Candidates/tp_Candidates/Program.cs
@@ -0,0 +1,168 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.EntityFrameworkCore;
+
+
+// условная бд с пользователями
+
+var people = new List<Person>
+{
+    new Person("tom@gmail.com", "12345"),
+    new Person("bob@gmail.com", "55555")
+};
+
+var builder = WebApplication.CreateBuilder();
+
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = AuthOptions.ISSUER,
+            ValidateAudience = true,
+            ValidAudience = AuthOptions.AUDIENCE,
+            ValidateLifetime = true,
+            IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
+            ValidateIssuerSigningKey = true
+        };
+    });
+
+
+// соединение с базой данных applicationdb
+string connection = "Server=(localdb)\\mssqllocaldb;Database=applicationdb;Trusted_Connection=True;";
+
+// добавляем класс ApplicationContext в сервисы приложения
+builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connection));
+
+
+
+var app = builder.Build();
+
+// подключаем функциональность статических файлов
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.UseAuthentication();   // добавление middleware аутентификации 
+app.UseAuthorization();   // добавление middleware авторизации 
+
+// app.Map("/", () => "Home Page");
+
+app.MapPost("/login", (Person loginData) =>
+{
+    // находим пользователя 
+    Person? person = people.FirstOrDefault(p => p.Email == loginData.Email && p.Password == loginData.Password);
+    // если пользователь не найден, отправляем статусный код 401
+    if (person is null) return Results.Unauthorized();
+
+    var claims = new List<Claim> { new Claim(ClaimTypes.Name, person.Email) };
+    // создаем JWT-токен
+    var jwt = new JwtSecurityToken(
+        issuer: AuthOptions.ISSUER,
+        audience: AuthOptions.AUDIENCE,
+        claims: claims,
+        expires: DateTime.UtcNow.Add(TimeSpan.FromMinutes(2)),
+        signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
+    var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+    // формируем ответ
+    var response = new
+    {
+        access_token = encodedJwt,
+        username = person.Email
+    };
+
+    return Results.Json(response);
+});
+// встраиваем компонент для подключения авторизации
+// app.Map("/hello", [Authorize] () => "Hello World!");
+app.Map("/data", [Authorize] () => new { message = "Hello World!" });
+
+
+/* начальные данные взяты с metanit.com/sharp/aspnet6/11.1.php
+затем добавлены элементы из различных статей с этого же сайта.
+раскоментировано и изменено самолично
+*/
+
+
+
+
+
+
+
+
+
+// отправление списка объектов Users по запросу через запрос в БД
+app.MapGet("/api/users", async (ApplicationContext db) => await db.Users.ToListAsync());
+
+// GET запрос // отправление одного объекта Users по айди, если объект существует, иначе 404
+app.MapGet("/api/users/{id:int}", async (int id, ApplicationContext db) =>
+{
+    // получаем пользователя по id
+    User? user = await db.Users.FirstOrDefaultAsync(u => u.Id == id);
+
+    // если не найден, отправляем статусный код и сообщение об ошибке
+    if (user == null) return Results.NotFound(new { message = "Пользователь не найден" });
+
+    // если пользователь найден, отправляем его
+    return Results.Json(user);
+});
+
+// DELETE запрос // удаление одного объекта Person по айди, если объект существует, иначе 404
+app.MapDelete("/api/users/{id:int}", async (int id, ApplicationContext db) =>
+{
+    // получаем пользователя по id
+    User? user = await db.Users.FirstOrDefaultAsync(u => u.Id == id);
+
+    // если не найден, отправляем статусный код и сообщение об ошибке
+    if (user == null) return Results.NotFound(new { message = "Пользователь не найден" });
+
+    // если пользователь найден, удаляем его
+    db.Users.Remove(user);
+    await db.SaveChangesAsync();
+    return Results.Json(user);
+});
+
+// POST запрос // создание пользователя (установка айди и добавление в список),
+// и отправление его обратно по запросу
+app.MapPost("/api/users", async (User user, ApplicationContext db) =>
+{
+    // добавляем пользователя в массив
+    await db.Users.AddAsync(user);
+    await db.SaveChangesAsync();
+    return user;
+});
+
+// PUT запрос // изменение пользователя по запросу, если пользователь существует, иначе 404
+app.MapPut("/api/users", async (User userData, ApplicationContext db) =>
+{
+
+    // получаем пользователя по id
+    var user = await db.Users.FirstOrDefaultAsync(u => u.Id == userData.Id);
+
+    // если не найден, отправляем статусный код и сообщение об ошибке
+    if (user == null) return Results.NotFound(new { message = "Пользователь не найден" });
+
+    // если пользователь найден, изменяем его данные и отправляем обратно клиенту
+    user.Age = userData.Age;
+    user.Name = userData.Name;
+    await db.SaveChangesAsync();
+    return Results.Json(user);
+});
+
+app.Run();
+
+
+
+
+
+/*
+public static AuthenticationBuilder AddAuthentication(this IServiceCollection services);
+public static AuthenticationBuilder AddAuthentication(this IServiceCollection services, string defaultScheme);
+public static AuthenticationBuilder AddAuthentication(this IServiceCollection services, Action>AuthenticationOptions> configureOptions);
+
+*/
