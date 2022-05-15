c1b33f6ca24c5dc8fc1d20ee51ac5822e7a9249e
Xarawg <92927559+Xarawg@users.noreply.github.com>
2022-05-15 17:28:56 +0500

    First upload

---
+++ tp_Candidates/tp_Candidates/Classes.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = ""; // имя пользователя
+    public int Age { get; set; } // возраст пользователя
+}
+
+public class AuthOptions
+{
+    public const string ISSUER = "MyAuthServer"; // издатель токена
+    public const string AUDIENCE = "MyAuthClient"; // потребитель токена
+    const string KEY = "mysupersecret_secretkey!123";   // ключ для шифрации
+    public static SymmetricSecurityKey GetSymmetricSecurityKey() =>
+        new SymmetricSecurityKey(Encoding.UTF8.GetBytes(KEY));
+}
+
+record class Person(string Email, string Password);
